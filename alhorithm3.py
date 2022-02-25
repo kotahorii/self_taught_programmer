e9a208d10fdc743fe141ac3ac264662c5af0e622
kotahorii <khorii.micron@gmail.com>
2022-02-25 23:36:46 +0900

    initial commit

--- alhorithm3.py
+++ alhorithm3.py
@@ -1,3 +1,6 @@
+import time
+
+
 def linear_search(a_list: list[int], n: int) -> bool:
     for i in a_list:
         if i == n:
@@ -6,6 +9,30 @@ def linear_search(a_list: list[int], n: int) -> bool:
 
 
 a_list = [1, 8, 32, 91, 5, 15, 9, 100, 3]
-print(linear_search(a_list, 91))
+start = time.time()
+print(linear_search(a_list, 3))
+end = time.time()
 
-list = 
+print(end - start)
+
+
+def binary_search(a_list: list[int], n: int) -> bool:
+    first = 0
+    last = len(a_list) - 1
+    while last >= first:
+        mid = (first + last) // 2
+        if a_list[mid] == n:
+            return True
+        else:
+            if n < a_list[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+    return False
+
+
+a_list = sorted(a_list)
+start = time.time()
+print(binary_search(a_list, 100))
+end = time.time()
+print(end - start)
