4f5c1ec40e9411a78a5255b4bfafb93a53369997
kotahorii <khorii.micron@gmail.com>
2022-02-26 15:09:12 +0900

    initial commit

--- algorithm6.py
+++ algorithm6.py
@@ -1,3 +1,7 @@
+import random
+from typing import Any
+
+
 def bubble_sort(a_list: list[str]) -> list[str]:
     list_length = len(a_list) - 1
     for i in range(list_length):
@@ -9,3 +13,52 @@ def bubble_sort(a_list: list[str]) -> list[str]:
         if no_swaps:
             return a_list
     return a_list
+
+
+def insertion_sort(a_list: list[Any]) -> list[Any]:
+    for i in range(1, len(a_list)):
+        value = a_list[i]
+        while i > 0 and a_list[i - 1] > value:
+            a_list[i] = a_list[i - 1]
+            i -= 1
+        a_list[i] = value
+    return a_list
+
+
+a_list = [random.randint(1, 1000) for _ in range(10)]
+print(insertion_sort(a_list))
+
+
+def merge_sort(a_list: list[Any]) -> list[Any]:
+    if len(a_list) > 1:
+        mid = len(a_list) // 2
+        left_half = a_list[:mid]
+        right_half = a_list[mid:]
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        left_ind = 0
+        right_ind = 0
+        alist_ind = 0
+        while left_ind < len(left_half) and right_ind < len(right_half):
+            if left_half[left_ind] <= right_half[right_ind]:
+                a_list[alist_ind] = left_half[left_ind]
+                left_ind += 1
+            else:
+                a_list[alist_ind] = right_half[right_ind]
+                right_ind += 1
+            alist_ind += 1
+
+        while left_ind < len(left_half):
+            a_list[alist_ind] = left_half[left_ind]
+            left_ind += 1
+            alist_ind += 1
+
+        while right_ind < len(right_half):
+            a_list[alist_ind] = right_half[right_ind]
+            right_ind += 1
+            alist_ind += 1
+    return a_list
+
+
+print(merge_sort(a_list))
