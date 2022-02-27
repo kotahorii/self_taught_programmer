0fa5b75634e6cde2a88fa6d914a918a83b3dd641
kotahorii <khorii.micron@gmail.com>
2022-02-27 15:55:11 +0900

    initial commit

--- algorizm9.py
+++ algorizm9.py
@@ -1,16 +1,27 @@
-def gcf(i1: int, i2: int) -> int:
-    if i1 == 0:
-        return i2
-    if i2 == 0:
-        return i1
+# def gcf(i1: int, i2: int) -> int:
+#     if i1 < 0 or i2 < 0:
+#         raise ValueError("Numbers must be positive")
+#     if i1 == 0:
+#         return i2
+#     if i2 == 0:
+#         return i1
 
-    if i1 > i2:
-        bigger = i1
-    else:
-        bigger = i2
-    for i in range(bigger // 2 + 1, 0, -1):
-        if i1 % i == 0 and i2 % i == 0:
-            return i
+#     if i1 > i2:
+#         bigger = i1
+#     else:
+#         bigger = i2
+#     for i in range(bigger // 2 + 1, 0, -1):
+#         if i1 % i == 0 and i2 % i == 0:
+#             return i
+#     raise ValueError("Numbers must be positive")
 
 
-print(gcf(9, 6))
+def gcf(x: int, y: int) -> int:
+    if y == 0:
+        x, y = y, x
+    while y != 0:
+        x, y = y, x % y
+    return x
+
+
+print(gcf(20, 12))
