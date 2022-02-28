d10e38236decab8ecca303098c401e3467bd1950
kotahorii <khorii.micron@gmail.com>
2022-02-28 21:08:03 +0900

    initial commit

--- data_structure1.py
+++ data_structure1.py
@@ -1,4 +1,19 @@
 import array
 
 arr = array.array("f", (1.0, 1.5, 2.0, 2.5))
-print(arr[1])
+
+
+def move_zeros(a_list: list[int]) -> list[int]:
+    zero_index = 0
+    for index, n in enumerate(a_list):
+        if n != 0:
+            a_list[zero_index] = n
+            if zero_index != index:
+                a_list[index] = 0
+            zero_index += 1
+    return a_list
+
+
+a_list = [8, 0, 3, 0, 12]
+move_zeros(a_list)
+print(a_list)
