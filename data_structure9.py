5b19287ce3f25767743a9d253e6c036092d0fe4d
kotahorii <khorii.micron@gmail.com>
2022-03-02 16:47:49 +0900

    initial commit

---
+++ data_structure9.py
@@ -0,0 +1,47 @@
+from typing import Any
+
+
+class BinaryTree:
+    def __init__(self, value: Any) -> None:
+        self.key = value
+        self.left_child = None
+        self.right_child = None
+
+    def insert_left(self, value: Any):
+        if self.left_child is None:
+            self.left_child = BinaryTree(value)
+        else:
+            bin_tree = BinaryTree(value)
+            bin_tree.left_child = self.left_child
+            self.left_child = bin_tree
+
+    def insert_right(self, value: Any):
+        if self.right_child is None:
+            self.right_child = BinaryTree(value)
+        else:
+            bin_tree = BinaryTree(value)
+            bin_tree.right_child = self.right_child
+            self.right_child = bin_tree
+
+    def breadth_first_search(self, n: Any) -> bool:
+        current = [self]
+        next: list[BinaryTree] = []
+        while current:
+            for node in current:
+                if node.key == n:
+                    return True
+                if node.left_child:
+                    next.append(node.left_child)
+                if node.right_child:
+                    next.append(node.right_child)
+            current = next
+            next = []
+        return False
+
+
+tree = BinaryTree(1)
+tree.insert_left(2)
+tree.insert_right(3)
+tree.insert_left(4)
+tree.left_child.insert_right(6)
+tree.insert_right(5)
